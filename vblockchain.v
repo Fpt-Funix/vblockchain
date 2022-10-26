70a60da1b6dbc34aa55105a0c274e92b9fad355b
habogay <habogay@gmail.com>
2022-10-26 10:55:51 +0700

    create new block

--- vblockchain.v
+++ vblockchain.v
@@ -1,5 +1,11 @@
 module main
 
+struct Block{
+	transactions []Transaction
+	previous_hash string
+	nonce int
+}
+
 struct Transaction {
 
 	hash string
@@ -13,6 +19,7 @@ struct Transaction {
 
 fn main() {
 	println('Hello World!')
+	// Create new Transaction
 	tx:=Transaction{
 		hash: '0x123',
 		sender: '0x456',
@@ -23,4 +30,22 @@ fn main() {
 
 	// tx.hash='0x456' // error: cannot assign to field `hash` of immutable binding `tx`
 	println(tx)
+
+	tx1:=Transaction{
+		hash: '0x1234',
+		sender: '0x4564',
+		recipient: '0x7894',
+		amount: 104,
+		timestamp: 1234567894
+	}
+	println(tx1)
+
+	// Create new Block
+	block:=Block{
+		transactions: [tx, tx1],
+		previous_hash: '0x123',
+		nonce: 123
+	}
+
+	println(block)
 }
