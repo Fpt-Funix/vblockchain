349004e82fc9d87cf93aa4a55b65471c35e992ae
habogay <habogay@gmail.com>
2022-10-26 17:00:48 +0700

    hash for transaction

--- core/transaction.v
+++ core/transaction.v
@@ -1,4 +1,5 @@
 module core
+import crypto.sha512
 
 pub struct Transaction {
 	hash string
@@ -6,4 +7,10 @@ pub struct Transaction {
 	recipient string
 	amount int
 	timestamp int
+	nonce int
+}
+
+pub fn (t Transaction) hash() string{
+	// nonce + from + amount + to + pre
+	return sha512.hexhash(t.nonce.str() + t.sender + t.amount.str() + t.recipient)
 }
