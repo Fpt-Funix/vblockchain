47d66f3050d41fcea4ba58b5273d9ca011c95aaa
habogay <habogay@gmail.com>
2022-10-31 12:06:32 +0700

    merge

--- core/blockchain.v
+++ core/blockchain.v
@@ -28,16 +28,11 @@ pub fn (blockchain Blockchain) check_chain_validity()  bool {
 	return true
 }
 
-// check nonce of the block
-pub fn (blockchain Blockchain) check_nonce_validity()  bool {
-	mut last_block := blockchain.chain[0]
-
-	return block.hash().starts_with('caffe')
-}
 
 
 pub fn (blockchain Blockchain) get_block_len()  int {
 	return blockchain.chain.len
+}
 
 pub fn (mut blockchain Blockchain) add_new_block(block Block)   {
 	if blockchain.validate_block(block) {
