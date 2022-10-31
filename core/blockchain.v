ae22631b77217e139198655a1998e52679fd2009
ngocbd <buidinhngoc.aiti@gmail.com>
2022-10-31 10:15:23 +0700

    example mine a block

--- core/blockchain.v
+++ core/blockchain.v
@@ -5,7 +5,7 @@ pub struct Blockchain {
 	pub mut : chain []Block  // because we want to add blocks to the chain
 	 current_transactions  []Transaction // list of pending transactions to be added to the next block
 	 queue  []Transaction  // list of transactions that are waiting to be added to the chain
-	 difficult int  // difficulty of the proof of work algorithm
+	 difficulty int = 1  // difficulty of the proof of work algorithm
 }
 pub fn (blockchain Blockchain) check_chain_validity()  bool {
 	mut last_block := blockchain.chain[0]
@@ -43,23 +43,17 @@ pub fn (mut blockchain Blockchain) add_new_block(block Block)   {
 
 pub fn (blockchain Blockchain) validate_block(block Block,) bool
 {
-	if block.index == 0
+	
+	candicate_hash := block.hash()
+	//println('candicate_hash ${candicate_hash}')
+	if candicate_hash.substr(0, 5*blockchain.difficulty) == 'caffe'
 	{
-		if block.previous_hash != '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000caffe'
-		{
-			return false
-		}
+		//println('block is valid')
+		return true
 	}
-	else
 	{
-		candicate_hash := block.hash()
-		if candicate_hash.substr(0, 5*blockchain.difficult) == 'caffe'
-		{
-			return true
-		}
-		{
-			return false
-		}
+		return false
 	}
+
 	return false
 }
